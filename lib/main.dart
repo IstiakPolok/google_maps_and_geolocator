a63f8312fed50732999c13904fa9cf9fd35f6c8c
IstiakPolok <fatin15-1944@diu.edu.bd>
2025-03-12 22:39:38 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'app/app.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
